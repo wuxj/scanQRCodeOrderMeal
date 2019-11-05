1ff7a56b86b549e49af8eb4fdae2510f4b88fd4c
吴下阿江 <780185916@qq.com>
2019-11-05 19:28:07 +0800

    init

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
 import Vue from 'vue'
 import App from './App'
-import store from './store'
-import config from './config/'
-const wind = uni.getSystemInfoSync().windowHeight;
 
 Vue.config.productionTip = false
-Vue.prototype.$store = store
-Vue.prototype.$config =  config
+
 App.mpType = 'app'
+
 const app = new Vue({
-	wind,
-	store,
-   ...App
+    ...App
 })
 app.$mount()
